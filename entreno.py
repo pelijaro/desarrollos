9d4dab9a3716024c1cb6ccbb51874d696e2a079d
Eduardo Jiménez <ejimenez@id-265.dom01.local>
2015-02-14 12:47:04 +0100

    mensaje semana dinamico

--- entreno.py
+++ entreno.py
@@ -3,6 +3,7 @@
 
 #Script acepta valores de pesos de diferentes ejercicios, formatea datos y sale en un archivo preparado para enviar email
 
+semana=0
 sentadilla=[]
 press_banca=[]
 dominadas=[]
@@ -20,44 +21,49 @@ print "¡Bienvenido a la interfaz de introducción de datos de entrenamiento! \n
 
 def semana_in():	
 	#En esta función se define la semana de la que se están introduciendo valores
-	semana=raw_input("Por favor, introduzca la semana correspondiente a los valores a añadir(valor numérico): ")
+	s=raw_input("Por favor, introduzca la semana correspondiente a los valores a añadir (valor numérico): ")
 
-	if (semana=="1" or semana=="2" or semana=="3" or semana=="4"):
+	if (s=="1" or s=="2" or s=="3" or s=="4"):
+		
+		#Con global, hago que asigne a la variable global y que no trate semana como una variable local
 
-		print "Semana correcta" #implementar la funcion que tenga que ir aqui, el print es de control
+		global semana
+		semana=s
+
+		print "\nSemana correcta\n" #implementar la funcion que tenga que ir aqui, el print es de control
 
 	else:
 
-		print "Semana incorrecta, introduzcala como se indica"
+		print "\nSemana incorrecta, introduzcala como se indica\n"
 		#Si la semana introducida es incorrecta, se vuelve a llamar a la función semana_in. 
 		semana_in()
 
 def sentadilla():
 
-def press_banca():
+	print "Por favor, introduzca los valores de las tres series de setandilla de la semana "+semana+": "
 
-def dominadas():
-
-def remo_menton():
-
-def sentadilla_hack():
-
-def remo_punta():
-
-def curl_biceps():
-
-def jalon_pecho():
-
-def extension_cuadri():
-
-def curl_femoral():
-
-def triceps_polea_cabeza():
-
-def encogimientos_mancu():
+semana_in()
+sentadilla()
 
 
+#def press_banca():
 
+#def dominadas():
 
+#def remo_menton():
 
+#def sentadilla_hack():
 
+#def remo_punta():
+
+#def curl_biceps():
+
+#def jalon_pecho():
+
+#def extension_cuadri():
+
+#def curl_femoral():
+
+#def triceps_polea_cabeza():
+
+#def encogimientos_mancu():
