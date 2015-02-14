8446f0b37ca82584bf01b7b8ee2f4c84f830c52e
Eduardo Jiménez <ejimenez@id-265.dom01.local>
2015-02-14 12:04:30 +0100

    creacion de variables globales y de funciones

--- entreno.py
+++ entreno.py
@@ -3,10 +3,23 @@
 
 #Script acepta valores de pesos de diferentes ejercicios, formatea datos y sale en un archivo preparado para enviar email
 
+sentadilla=[]
+press_banca=[]
+dominadas=[]
+remo_menton=[]
+sentadilla_hack=[]
+remo_punta=[]
+curl_biceps=[]
+jalon_pecho=[]
+extension_cuadri=[]
+curl_femoral=[]
+triceps_polea_cabeza=[]
+encogimientos_mancu=[]
+
 print "¡Bienvenido a la interfaz de introducción de datos de entrenamiento! \n"
 
 def semana_in():	
-
+	#En esta función se define la semana de la que se están introduciendo valores
 	semana=raw_input("Por favor, introduzca la semana correspondiente a los valores a añadir(valor numérico): ")
 
 	if (semana=="1" or semana=="2" or semana=="3" or semana=="4"):
@@ -19,5 +32,32 @@ def semana_in():
 		#Si la semana introducida es incorrecta, se vuelve a llamar a la función semana_in. 
 		semana_in()
 
-semana_in()
+def sentadilla():
+
+def press_banca():
+
+def dominadas():
+
+def remo_menton():
+
+def sentadilla_hack():
+
+def remo_punta():
+
+def curl_biceps():
+
+def jalon_pecho():
+
+def extension_cuadri():
+
+def curl_femoral():
+
+def triceps_polea_cabeza():
+
+def encogimientos_mancu():
+
+
+
+
+
 
